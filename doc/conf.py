ec5b4eb3a4fc01cdbb50a47a3a605b5b5ec0fd2e
Linus Aldebjer <aldebjer@gmail.com>
2016-07-19 19:55:45 +0200

    Using rtd theme

--- doc/conf.py
+++ doc/conf.py
@@ -105,29 +105,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 
-sspalightblue = "#007CC0"
-sspadarkblue = "#005B9A"
-sspagrey = "#949494"
-lightgrey = "#F0F0F0"
-sspaorange = "#EF8E45"
-
-html_theme_options = {"footerbgcolor": sspagrey,
-                      "sidebarbgcolor": sspalightblue,
-                      "sidebartextcolor": "white",
-                      "sidebarlinkcolor":"white",
-                      "relbarbgcolor": sspadarkblue,
-                      "visitedlinkcolor": sspaorange,
-                      "linkcolor":sspaorange,
-                      "headbgcolor":"white",
-                      "headtextcolor":sspagrey,
-                      "codebgcolor": lightgrey
-                      }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -151,7 +134,7 @@ html_theme_options = {"footerbgcolor": sspagrey,
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
