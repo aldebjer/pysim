de001ef909d73f3ce55636421c39173c90d90f37
Linus Aldebjer <aldebjer@gmail.com>
2016-09-20 14:32:24 +0200

    pylint formatting fixes

--- pysim/systems/__init__.py
+++ pysim/systems/__init__.py
@@ -1,5 +1,7 @@
 """Contains default systems that can be used in simulations
 """
+# Copyright should be placed above imports
+#pylint: disable=wrong-import-position
 __copyright__ = 'Copyright (c) 2014-2016 SSPA Sweden AB'
 
 #The pylint warning is temporarily disabled here, should be enabled once the
