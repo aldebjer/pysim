d96efe4af1e77e227fff49d162b63db3e869618c
Linus Aldebjer <aldebjer@gmail.com>
2017-03-08 15:39:04 +0100

    loading systems from json file

--- msvc/cpp_runner/main.cpp
+++ msvc/cpp_runner/main.cpp
@@ -2,31 +2,85 @@
 #include "CppSystem.hpp"
 #include "StoreHandler.hpp"
 
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+#include <sstream>
+#include <boost/algorithm/string/replace.hpp>
+
 #include <iostream>
 #include <windows.h>
 
+namespace pt = boost::property_tree;
+
 typedef pysim::CppSystem*(__cdecl *funcpt)(char* name);
 
+struct systeminfo {
+    std::string name;
+    std::string module;
+    std::vector<std::pair<std::string, std::double_t>> inputs;
+    std::vector<std::string> stores;
+};
+
 int main(int argc, char *argv[]) {
-    HINSTANCE hGetProcIDDLL = LoadLibrary("C:\\dev\\pysims\\pysim\\pysim\\systems\\defaultsystemcollection1.cp35-win32.pyd");
-    if (!hGetProcIDDLL) {
-        std::cout << "could not load the dynamic library" << std::endl;
-        return 1;
+    using std::string;
+    using std::pair;
+
+    pt::ptree root;
+    pt::read_json("simplesim.json", root);
+
+    std::vector<systeminfo> system_infos;
+
+    for (pt::ptree::value_type &json_system : root.get_child("systems")) {
+        systeminfo sys;
+        sys.name = json_system.first;
+        sys.module = json_system.second.get_child("module").data();
+        for (pt::ptree::value_type & json_input : json_system.second.get_child("inputs")) {
+            string key = json_input.first;
+            string value(json_input.second.data());
+            sys.inputs.push_back(std::make_pair(key, std::stod(value)));
+        }
+        for (pt::ptree::value_type & json_store : json_system.second.get_child("stores")) {
+            sys.stores.push_back(json_store.second.data());
+        }
+        system_infos.push_back(sys);
     }
 
-    funcpt funci = (funcpt)GetProcAddress(hGetProcIDDLL, "getCppSystem");
-    if (!funci) {
-        std::cout << "could not locate the function" << std::endl;
-        return 1;
+    std::map<string, funcpt> factorymap;
+    for (systeminfo &sys : system_infos) {
+        if (factorymap.count(sys.module) == 0) {
+            std::stringstream ss;
+            ss << "C:\\dev\\pysims\\pysim\\";
+            string module(sys.module);
+            boost::algorithm::replace_all(module, ".", "\\");
+            ss << module << ".cp35-win32.pyd";
+            HINSTANCE hGetProcIDDLL = LoadLibrary(ss.str().c_str());
+            if (!hGetProcIDDLL) {
+                std::cout << "could not load the dynamic library" << std::endl;
+                return 1;
+            }
+            funcpt funci = (funcpt)GetProcAddress(hGetProcIDDLL, "getCppSystem");
+            if (!funci) {
+                std::cout << "could not locate the function" << std::endl;
+                return 1;
+            }
+            factorymap[sys.module] = funci;
+        }
     }
 
-    pysim::CppSystem* sys = funci("VanDerPol");
-    sys->store("x");
     pysim::Simulation sim;
-    sim.addSystem(sys);
+    std::vector<pysim::CppSystem*> systems;
+
+    for (systeminfo& sys_info : system_infos) {
+        pysim::CppSystem* sys = factorymap[sys_info.module]("VanDerPol");
+        systems.push_back(sys);
+        sim.addSystem(sys);
+    }
+
+
     sim.simulate(200, 0.1,"rk4",0.1,0.1,false);
-    std::vector<double> v;
-    v = sys->getStoreHandlerP()->getStoreVector("x");
-    std::cout << v.at(20) << std::endl;
+    //std::vector<double> v;
+    //v = sys->getStoreHandlerP()->getStoreVector("x");
+    //std::cout << v.at(20) << std::endl;
 
 }
