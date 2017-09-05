b9168a10c833ded4bbc5e1c7fafb68cdc4bd3177
Linus Aldebjer <aldebjer@gmail.com>
2017-09-05 12:47:15 +0200

    Removing dead code

--- pysim/cppsource/CompositeSystemImpl_p.hpp
+++ pysim/cppsource/CompositeSystemImpl_p.hpp
@@ -27,12 +27,6 @@ struct CompositeSystemImplPrivate {
     std::vector<SimulatableSystemInterface*> subsystems;
     std::map<std::string, SimulatableSystemInterface*> subsystems_map;
 
-    //std::vector<CommonSystemImpl*> subsystems_common;
-    //std::map<std::string, CommonSystemImpl*> subsystems_common_map;
-
-    //std::vector<CompositeSystemImpl*> subsystems_composite;
-    //std::map<std::string, CompositeSystemImpl*> subsystems_composite_map;
-
     std::vector<std::unique_ptr<double>> scalar_inports;
     std::vector<std::unique_ptr<pysim::vector>> vector_inports;
     std::vector<std::unique_ptr<Eigen::MatrixXd>> matrix_inports;
