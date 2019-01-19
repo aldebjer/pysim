fcd49284415e6e9174f7af320986880a3f005d57
Linus Aldebjer <linus@aldebjer.se>
2019-01-19 23:00:53 +0100

    geting latest version of Eigen

--- get_external.py
+++ get_external.py
@@ -1,15 +1,24 @@
+"""Download the Eigen vector library and unzip it into the current 
+directory.
+"""
+
 from urllib.request import urlopen
 from io	 import BytesIO
 import zipfile
 
-#Open the URL to download Eigen 3.3
-url = "http://bitbucket.org/eigen/eigen/get/3.3.0.zip"
-remotedata = urlopen(url)
+def read_and_unpack():
+    """Reand and unpack Eigen to the current directory"""
+    #Open the URL to download Eigen 3.3
+    url = "http://bitbucket.org/eigen/eigen/get/3.3.7.zip"
+    remotedata = urlopen(url)
 
-#Convert the downloaded data to an in memory file-like object
-zipdata = BytesIO()
-zipdata.write(remotedata.read())
+    #Convert the downloaded data to an in memory file-like object
+    zipdata = BytesIO()
+    zipdata.write(remotedata.read())
 
-#Create a zipfile and extract it
-myzipfile = zipfile.ZipFile(zipdata)
-myzipfile.extractall()
+    #Create a zipfile and extract it
+    myzipfile = zipfile.ZipFile(zipdata)
+    myzipfile.extractall()
+
+if __name__ == "__main__":
+    read_and_unpack()
