d7d305fceaddb2549c742e6a0fd91bf0c420c62d
freol35241 <freol@outlook.com>
2019-01-21 21:56:00 +0100

    SimulatableSystemInterface now uses "only" __functions

--- pysim/cppsource/SimulatableSystem.hpp
+++ pysim/cppsource/SimulatableSystem.hpp
@@ -9,17 +9,14 @@ public:
     virtual ~SimulatableSystemInterface(void){};
 
 	virtual void __preSim() = 0;
-	virtual void preSim() {};
 	virtual void __preStep() = 0;
-	virtual void preStep() {};
 	virtual void __doStep(double time) = 0;
-    virtual void doStep(double time)=0;
 	virtual void __postStep() = 0;
-    virtual void postStep(){};
 
     virtual void doStoreStep(double time) = 0;
-    virtual void copyoutputs() = 0;
-    virtual void copystateoutputs() = 0;
+	virtual void __copyinputs() = 0;
+    virtual void __copyoutputs() = 0;
+    virtual void __copystateoutputs() = 0;
 
     virtual std::vector<double*> getStatePointers() = 0;
     virtual std::vector<double*> getDerPointers() = 0;
