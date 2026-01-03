bd15cc809eb986ef60c461a43276798db45dc9f5
Linus Aldebjer <linus@aldebjer.se>
2026-01-03 16:42:31 +0100

    Build on Windows

--- get_external.py
+++ get_external.py
@@ -1,5 +1,5 @@
-"""Download the Boost and Eigen library and unzip it into the current 
-directory.
+"""Download the Boost library and unzip it into the external directory.
+For use on the windows OS.
 """
 
 from urllib.request import urlopen
@@ -7,7 +7,7 @@ from io	 import BytesIO
 import zipfile
 
 def read_and_unpack(url):
-    """Read and unpack a zip file to the current directory"""
+    """Read and unpack a zip file to the 'external' directory"""
 
     remotedata = urlopen(url)
 
@@ -20,5 +20,4 @@ def read_and_unpack(url):
     myzipfile.extractall(path='external')
 
 if __name__ == "__main__":
-    read_and_unpack("https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.zip")
-    read_and_unpack("https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.3.7.zip")
+    read_and_unpack("https://archives.boost.io/release/1.90.0/source/boost_1_90_0.zip")
